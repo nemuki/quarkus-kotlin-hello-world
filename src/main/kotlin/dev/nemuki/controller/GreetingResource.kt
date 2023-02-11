cb3a36c195ac8132c88d6d56f8a284be9f18c01b
Naoki Takahashi <nemuki@users.noreply.github.com>
2023-02-11 22:07:08 +0900

    レスポンスを簡略化

--- src/main/kotlin/dev/nemuki/controller/GreetingResource.kt
+++ src/main/kotlin/dev/nemuki/controller/GreetingResource.kt
@@ -7,7 +7,6 @@ import org.jboss.resteasy.reactive.RestQuery
 import javax.validation.constraints.Size
 import javax.ws.rs.GET
 import javax.ws.rs.Path
-import javax.ws.rs.core.Response
 
 @Path("/hello")
 class GreetingResource(
@@ -16,9 +15,8 @@ class GreetingResource(
     @GET
     fun hello(
         @Size(min = 3, max = 10) @RestQuery name: String?
-    ): Response {
+    ): Greeting {
         Log.debug("request name: $name")
-        return Response.status(Response.Status.ACCEPTED).entity(Greeting(message = "hello $text, ${name ?: "world"}"))
-            .build()
+        return Greeting(message = "hello $text, ${name ?: "world"}")
     }
 }
