5ca1760a647d6854d7bdd96f06ef50afda29f3cf
Naoki Takahashi <nemuki@users.noreply.github.com>
2023-02-11 21:59:13 +0900

    Greeting に DEBUG ログ出力を追加

--- src/main/kotlin/dev/nemuki/controller/GreetingResource.kt
+++ src/main/kotlin/dev/nemuki/controller/GreetingResource.kt
@@ -1,11 +1,13 @@
 package dev.nemuki.controller
 
 import dev.nemuki.entity.Greeting
+import io.quarkus.logging.Log
 import org.eclipse.microprofile.config.inject.ConfigProperty
 import org.jboss.resteasy.reactive.RestQuery
 import javax.validation.constraints.Size
 import javax.ws.rs.GET
 import javax.ws.rs.Path
+import javax.ws.rs.core.Response
 
 @Path("/hello")
 class GreetingResource(
@@ -13,8 +15,10 @@ class GreetingResource(
 ) {
     @GET
     fun hello(
-        @Size(min = 3, max = 10)
-        @RestQuery
-        name: String?
-    ) = Greeting(message = "hello $text, ${name ?: "world"}")
+        @Size(min = 3, max = 10) @RestQuery name: String?
+    ): Response {
+        Log.debug("request name: $name")
+        return Response.status(Response.Status.ACCEPTED).entity(Greeting(message = "hello $text, ${name ?: "world"}"))
+            .build()
+    }
 }
